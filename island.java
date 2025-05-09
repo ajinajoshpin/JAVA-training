2762da64b13ea9f774eb771922e75d2ffdc8e7e4
ajinajoshpin <148514578+ajinajoshpin@users.noreply.github.com>
2025-05-09 18:44:57 +0530

    Create island.java

---
+++ island.java
@@ -0,0 +1,34 @@
+public class island {
+
+    public static int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++)
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+        return count;
+    }
+
+    static void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
+            return;
+        
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+// array is call by reference
+    public static void main(String[] args) {
+        char[][] grid = {
+            {'1','1','1','1','0'},
+            {'1','1','0','1','0'},
+            {'1','1','0','0','0'},
+            {'0','0','0','0','0'}
+        };
+        System.out.println(numIslands(grid)); // Output: 1
+    }
+}
